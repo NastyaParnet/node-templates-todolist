c27d950e5b00b90cf8f1ec84264e4082d454203c
NastyaParnet <nastya.parnet.01@gmail.com>
2024-09-05 10:47:23 +0300

    update name PORT value

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.post('/delete/:index', (req, res) => {
   res.redirect('/');
 });
 
-const server = app.listen(process.env.port || 3000);
+const server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
